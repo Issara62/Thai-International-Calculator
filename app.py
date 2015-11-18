63610277c37307fff1ee3997a30a239e2d8f07aa
Issara Singtothong <isingtothong@hotmail.com>
2015-11-18 17:25:08 +1000

    First Commit

---
+++ app.py
@@ -0,0 +1,17 @@
+__author__ = 'Singtothong'
+
+from kivy.app import App
+from kivy.lang import Builder
+
+
+
+
+class ThaiInterCalc(App):
+    def __init__(self):
+        def __init__(self, **kwargs):
+            super().__init__(**kwargs)
+
+    def build(self):
+        pass
+
+
